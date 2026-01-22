99ee647ad3cd1619129878580ddace1aab4ced85
dvtinh99 <tinh.doan@codecomplete.jp>
2026-01-22 17:07:25 +0700

    add sheet

--- resources/js/pages/Sample.vue
+++ resources/js/pages/Sample.vue
@@ -17,6 +17,21 @@ import {
   DialogTrigger,
   DialogClose,
 } from '@/components/ui/dialog'
+
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet'
+
+
 import { Button } from '@/components/ui/button'
 const isLoading = ref(false)
 const nodes = ref<any[]>([
@@ -125,6 +140,41 @@ function onNodeClick({ event, node }) {
 
 <template>
   <div class="h-screen w-screen" v-if="!isLoading">
+  <Sheet>
+    <SheetTrigger as-child>
+      <Button variant="outline">
+        Open
+      </Button>
+    </SheetTrigger>
+    <SheetContent>
+      <SheetHeader>
+        <SheetTitle>Edit profile</SheetTitle>
+        <SheetDescription>
+          Make changes to your profile here. Click save when you're done.
+        </SheetDescription>
+      </SheetHeader>
+      <div class="grid flex-1 auto-rows-min gap-6 px-4">
+        <div class="grid gap-3">
+          <Label for="sheet-demo-name">Name</Label>
+          <Input id="sheet-demo-name" default-value="Pedro Duarte" />
+        </div>
+        <div class="grid gap-3">
+          <Label for="sheet-demo-username">Username</Label>
+          <Input id="sheet-demo-username" default-value="@peduarte" />
+        </div>
+      </div>
+      <SheetFooter>
+        <Button type="submit">
+          Save changes
+        </Button>
+        <SheetClose as-child>
+          <Button variant="outline">
+            Close
+          </Button>
+        </SheetClose>
+      </SheetFooter>
+    </SheetContent>
+  </Sheet>
   <Dialog>
     <!-- Nút mở dialog -->
     <DialogTrigger as-child>
